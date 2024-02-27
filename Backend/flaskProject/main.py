047ec7aa8bd18bf41e04f5807144f3ca23a5f268
Chen Chu <cchu37@asu.edu>
2024-02-26 23:15:53 -0700

    task-116 done

--- Backend/flaskProject/main.py
+++ Backend/flaskProject/main.py
@@ -10,9 +10,10 @@ from taigaApi.userStory.getUserStory import get_user_story
 from taigaApi.task.getTaskHistory import get_task_history
 from taigaApi.task.getTasks import get_closed_tasks, get_all_tasks, get_one_closed_task, get_tasks, get_closed_tasks_for_a_sprint
 from taigaApi.project.getProjectMilestones import get_number_of_milestones, get_milestone_id
-from taigaApi.milestones.getMilestonesForSprint import get_milestones_by_sprint
+from taigaApi.milestones.getMilestonesForSprint import get_milestones_by_sprint, get_milestone_stats_by_sprint
 from taigaApi.task.getTasks import get_lead_times_for_tasks, get_userstories_for_milestones
 from taigaApi.customAttributes.getCustomAttributes import get_business_value_data_for_sprint, get_business_value_id, get_user_story_business_value_map, get_custom_attribute_values
+from taigaApi.userStory.getUserStory import get_user_story_start_date
 import secrets
 import requests
 from threading import Thread
@@ -495,30 +496,98 @@ def render_error():
     return render_template("error.html")
 
 @app.route("/VIP", methods=["GET"])
-def render_VIP():
+def render_VIP_page():
     if "auth_token" not in session:
         return redirect("/")
     return render_template("VIP.html")
 
-@app.route("/VIP", methods=["POST"])
-def get_VIP():
+@app.route("/VIPC", methods=["GET"])
+def calculate_VIP():
     if "auth_token" not in session:
         return redirect("/")
-    if request.method == "POST":
-        #get all the user stories from the sprint
-        user_stories = get_userstories_for_milestones(session['sprint_id'], session['auth_token'])#it has complete infromation
-        get_userstory_ids = lambda: [userstory['id'] for userstory in user_stories]
-        userstory_ids = get_userstory_ids()
-        business_value_id = get_business_value_id(session["project_id"], session["auth_token"])
-        custom_attribute_values = get_custom_attribute_values(userstory_ids, session['auth_token'])
-        user_story_business_value_map = get_user_story_business_value_map(business_value_id, custom_attribute_values)
-        total_business_value = sum(user_story_business_value_map.values())
+    #get all the user stories from the sprint
+    user_stories = get_userstories_for_milestones([session['sprint_id']], session['auth_token'])[0]#it has complete infromation
 
-        #get total user stories points
-        total_points = 0
-        for story in user_stories:
-            if (story["total_points"] != None):
-                total_points += story["total_points"]
+    #get business value of each user story, and calculate total business value
+    get_userstory_ids = lambda: [userstory['id'] for userstory in user_stories]
+    userstory_ids = get_userstory_ids()
+    business_value_id = get_business_value_id(session["project_id"], session["auth_token"])
+    custom_attribute_values = get_custom_attribute_values(userstory_ids, session['auth_token'])
+    ###
+    user_story_business_value_map = get_user_story_business_value_map(business_value_id, custom_attribute_values)
+    print("user_story_business_value_map")
+    print(user_story_business_value_map)
+    total_business_value = sum(user_story_business_value_map.values())
+    ###
+
+    #get total user stories points
+
+    ####
+    total_points = 0
+    story_points_map = {}
+    story_finish_date_map = {}
+    ####
+
+    for story in user_stories:
+        if (story["total_points"] != None):
+            total_points += story["total_points"]
+            story_points_map[story["id"]] = story["total_points"]
+            story_finish_date_map[story["id"]] = story["finish_date"]
+
+    print("story_points_map")
+    print(story_points_map)
+
+    #get story start dates
+    ###
+    story_start_date_map = get_user_story_start_date(user_stories, session['auth_token'])
+    ###
+
+    #get starting date of a sprint
+    #get ending date of a sprint "finish_date"
+    sprint_data = get_milestones_by_sprint (session['project_id'], session['sprint_id'], session['auth_token'])
+    sprint_start_date =  sprint_data["estimated_start"]
+    sprint_start_date = datetime.fromisoformat(sprint_start_date)
+    sprint_finish_date = sprint_data["estimated_finish"]
+    sprint_finish_date = datetime.fromisoformat(sprint_finish_date)
+    """
+    data_to_plot = { 
+            "total_story_points": total_points,
+            "x_axis": [],
+            "y_axis": [],
+            "sprint_start_date": sprint_start_date,
+            "sprint_end_date": sprint_finish_date,
+    }
+
+    data_to_plot["x_axis"] = [(sprint_start_date + timedelta(days=day)).strftime("%d %b %Y") for day in range((sprint_finish_date - sprint_start_date).days + 1)]
+    data_to_plot["y_axis"] = [i for i in range(0, data_to_plot["total_story_points"] + 20, 10)]
+    """
+    #check which user stories is in progress at a given date
+    date_list = [(sprint_start_date + timedelta(days=day)) for day in range((sprint_finish_date - sprint_start_date).days + 1)]
+    data_points = []
+    one_day_points = 0
+    one_day_BV = 0
+    for date in date_list:
+        date = date.replace(tzinfo=None)
+        for user_story in user_stories:
+            if story_start_date_map[user_story["id"]] == None:
+                continue
+            date1 = story_start_date_map[user_story["id"]].replace(tzinfo=None)
+            if story_finish_date_map[user_story["id"]] != None:
+                date2 = datetime.fromisoformat(story_finish_date_map[user_story["id"]]).replace(tzinfo=None)
+            else:
+                date2 = None
+            if date1 <= date and (date2 == None or date2 >= date):
+                one_day_points += story_points_map[user_story["id"]]
+                one_day_BV += user_story_business_value_map[user_story["id"]]
+
+        data_points.append({
+            "date": date.strftime("%d %b %Y"), 
+            "user_story_points": one_day_points/total_points, 
+            "BV": one_day_BV/total_business_value
+            })
+        one_day_points = 0
+        one_day_BV = 0
         
-        #check whic user stories is in progress at a given date
-        
+        print(data_points)
+
+    return jsonify(data_points)
