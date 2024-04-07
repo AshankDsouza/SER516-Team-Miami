847f5d5a79765d3216fdd4c945d4e1eb882c4399
Manoj <mmadhus2@asu.edu>
2024-04-07 11:33:51 -0700

    Test for partial work done

---
+++ microservice_tests/partialworkdone_test.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest.mock import patch, MagicMock
+from Backend.flaskProject.main import app
+
+class PartialWorkDoneTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    @patch('Backend.flaskProject.main.requests.get')
+    def twd_test(self, fake_post):
+        with self.app.session_transaction() as fake_session:
+            fake_session['auth_token'] = 'fake_auth_token'
+        fake_response = MagicMock()
+        fake_response.status_code = 200
+        fake_response.json.return_value = {'data_to_plot': 'fake_data_to_plot'}
+        fake_post.return_value = fake_response
+        response = self.app.get('/partial-work-done-chart')
+        self.assertEqual(response.status_code, 200)
