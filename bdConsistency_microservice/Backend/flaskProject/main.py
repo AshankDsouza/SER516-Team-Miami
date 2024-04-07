7dac5f1494be81e35b25d2cbc5b4ed68e7b21d36
Manoj <mmadhus2@asu.edu>
2024-04-07 16:48:05 -0700

    Completed implementing microservice for BD-Calculation

--- bdConsistency_microservice/Backend/flaskProject/main.py
+++ bdConsistency_microservice/Backend/flaskProject/main.py
@@ -53,14 +53,59 @@ def uiRender():
     return render_template("BD-Consistency-graph.html")
 
 
-# @app.route("/<project_id>/<sprint_id>/<auth_token>/partial-work-done-chart", methods=["GET"])
-# def partialWrokDone(project_id, sprint_id, auth_token):
-#     try:
-#         data_to_plot = partialWorkDone(project_id, sprint_id, auth_token)
+@app.route("/<project_id>/<sprint_id>/<auth_token>/bd-calculation", methods=["GET"])
+def calclateBDConsistency(project_id, sprint_id, auth_token):
+    try:
+        # Data required to plot is stored here
+        data_to_plot = { 
+                "total_story_points": 0,
+                "x_axis": [],
+                "bv_projection": [],
+                "story_points_projection": []
+        }
 
-#         return json.dumps({"data_to_plot": data_to_plot }), 200
-#     except Exception as e:
-#         # Handle errors during the API request and print an error message
-#         print(e)
-#         return json.dumps({ "msg": "Failed plot sprint data" }), 500
+        milestone = get_milestones_by_sprint(project_id, sprint_id, auth_token)
+
+        for user_story in milestone["user_stories"]:
+            if user_story["total_points"] == None: 
+                continue
+
+            data_to_plot["total_story_points"] += int(user_story["total_points"])
+                
+        start_date  = datetime.strptime(milestone["estimated_start"], '%Y-%m-%d')
+        end_date = datetime.strptime(milestone["estimated_finish"], '%Y-%m-%d')
+
+        data_to_plot["x_axis"] = [(start_date + timedelta(days=day)).strftime("%d %b %Y") for day in range((end_date - start_date).days + 1)]
+
+        for index in range(0, len(data_to_plot["x_axis"])):
+            current_processing_date = data_to_plot["x_axis"][index]
+            current_processing_date_points = 0
+
+            if index <= 0:
+                current_processing_date_points = data_to_plot["total_story_points"]
+            else:
+                current_processing_date_points = data_to_plot["story_points_projection"][index - 1]
+
+            total_points_completed = 0
+            for user_story in milestone["user_stories"]:
+                if(user_story["finish_date"] == None or user_story["total_points"] == None):
+                    continue
+
+                finish_date = datetime.fromisoformat(user_story["finish_date"]).strftime("%d %b %Y")
+                        
+                if(finish_date != current_processing_date):
+                    continue
+
+                total_points_completed = user_story["total_points"] + total_points_completed
+
+            data_to_plot["story_points_projection"].append(round(current_processing_date_points - total_points_completed, 1))
+
+        running_bv_data, _ = get_business_value_data_for_sprint(project_id, sprint_id, auth_token)
+        data_to_plot['bv_projection'] = running_bv_data
+
+        return jsonify({ "data_to_plot": data_to_plot }), 200
+    except Exception as e:
+        # Handle errors during the API request and print an error message
+        print(e)
+        return json.dumps({ "msg": "Failed plot bd consistency data" }), 500
     
